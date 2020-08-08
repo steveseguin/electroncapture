6116cd61421a95e007fa4fa7fe84ce85a9d90703
STeve Seguin <steve@seguin.email>
2020-08-08 03:55:19 -0400

    fixed args I hope

--- main.js
+++ main.js
@@ -3,50 +3,41 @@ const electron = require('electron')
 const {app, BrowserWindow, ipcMain, screen} = require('electron')
 const path = require('path')
 const process = require('process')
-const yargs = require('yargs')
 
-
-const argv = yargs
-    .command('width', 'Sets the WIDTH in pixels', {
-        width: {
-            description: 'Sets the width in pixels',
-            alias: 'w',
-            type: 'number',
-        }
-    })
-	.command('height', 'Sets the HEIGHT in pixels', {
-        height: {
-            description: 'Sets the height in pixels',
-            alias: 'h',
-            type: 'number',
-        }
-    })
-	.command('url', 'Sets the URL to load', {
-        url: {
-            description: 'Sets the URL to loads',
-            alias: 'u',
-            type: 'string',
-        }
-    })
-    .help()
-    .alias('help', 'h')
-    .argv;
+var { argv } = require("yargs")
+  .scriptName("area")
+  .usage("Usage: $0 -w num -h num -w string")
+  .example(
+    "$0 -w 1280 -h 720 -u https://obs.ninja/?view=xxxx",
+    "Loads the stream with ID xxxx into a window sized 1280x720"
+  )
+  .option("w", {
+    alias: "width",
+    describe: "The width of the window in pixel.",
+    type: "number",
+    nargs: 1,
+  })
+  .option("h", {
+    alias: "height",
+    describe: "The height of the window in pixels.",
+    type: "number",
+    nargs: 1,
+  })
+  .option("u", {
+    alias: "url",
+    describe: "The URL of the window to load.",
+    type: "string",
+    nargs: 1,
+  })
+  .describe("help", "Show help.") // Override --help usage message.
+  .default("h", 720)
+  .default("w", 1280)
+  .default("u", "https://obs.ninja/electron")
+  
+const { width, height, url } = argv;
 
 function createWindow () {
-  let url = "https://obs.ninja/electron";
-  let width = 1280;
-  let height = 720;
-
-  if (argv._.includes('url')) {
-    url = argv.url || "https://obs.ninja/electron";
-  }
-  if (argv._.includes('height')) {
-    height = argv.height || 720;
-  }
-  if (argv._.includes('width')) {
-    width = argv.width || 1280;
-  }
-  
+ 
   const screen = electron.screen
   let factor = screen.getPrimaryDisplay().scaleFactor;
   // Create the browser window.
