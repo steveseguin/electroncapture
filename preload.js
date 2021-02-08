49c81915030209aa2cd03b7e97d96976c79a10db
STeve Seguin <steve@seguin.email>
2021-02-07 21:33:13 -0500

    global hotkey mute

--- preload.js
+++ preload.js
@@ -1,5 +1,5 @@
-// All of the Node.js APIs are available in the preload process.
-// It has the same sandbox as a Chrome extension.
+const { ipcRenderer } = require('electron')
+
 window.addEventListener('DOMContentLoaded', () => {
   const replaceText = (selector, text) => {
     const element = document.getElementById(selector)
@@ -9,4 +9,28 @@ window.addEventListener('DOMContentLoaded', () => {
   for (const type of ['chrome', 'node', 'electron']) {
     replaceText(`${type}-version`, process.versions[type])
   }
+})
+
+
+
+window.addEventListener('message', ({ data }) => {
+  ipcRenderer.send('postMessage', data)
+})
+
+ipcRenderer.on('postMessage', (event, ...args) => {
+	try{
+		if ("mic" in args[0]) { // this should work for the director's mic mute button as well. Needs to be manually enabled the first time still tho.
+			if (args[0].mic === true) { // unmute
+				session.muted = false; // set
+				toggleMute(true); // apply 
+			} else if (args[0].mic === false) { // mute
+				session.muted = true; // set
+				toggleMute(true); // apply
+			} else if (args[0].mic === "toggle") { // toggle
+				toggleMute();
+			}
+		}
+	}catch(e){
+		console.error(e);
+	}
 })
