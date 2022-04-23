922bcf3dc90b8e72167f4aedb165346dbe9b7535
Steve Seguin <steveseguin@Steves-Air.seguiin.home>
2022-04-23 16:02:26 -0400

    v100

--- afterSign.js
+++ afterSign.js
@@ -10,7 +10,7 @@ module.exports = async function (params) {
     console.log('afterSign hook triggered', params);
 
     // Same appId in electron-builder.
-    let appId = 'ninja.obs'
+    let appId = 'ninja.vdo'
 
     let appPath = path.join(params.appOutDir, `${params.packager.appInfo.productFilename}.app`);
     if (!fs.existsSync(appPath)) {
