8046390f3e329621df47847815cb2439c068c2ac
SteveSeguin <punkrawker@gmail.com>
2022-06-06 11:09:20 -0400

    removing some elevated priv depedencies

--- preload.js
+++ preload.js
@@ -1,4 +1,4 @@
-const { ipcRenderer } = require('electron')
+const { ipcRenderer, contextBridge } = require('electron');
 
 window.addEventListener('DOMContentLoaded', () => {
   const replaceText = (selector, text) => {
@@ -9,74 +9,161 @@ window.addEventListener('DOMContentLoaded', () => {
   for (const type of ['chrome', 'node', 'electron']) {
     replaceText(`${type}-version`, process.versions[type])
   }
+  
+	try{
+		if (session && session.version){
+			ipcRenderer.send('vdonVersion', {ver:session.version});
+		}
+	} catch(e){}
+  
 })
 
 window.addEventListener('message', ({ data }) => {
-	console.log("MessageIncoming:15:"+data);
+	console.log("preload.js-Message-Incoming: "+data);
     ipcRenderer.send('postMessage', data)
-})
+});
 
+
+var doSomethingInWebApp = null;
+
+ipcRenderer.send('vdonVersion', {ver:false}); // clear the current Version; let it load if needed.
+try {
+	contextBridge.exposeInMainWorld("electronApi", { // 
+	  'exposeDoSomethingInWebApp' : function (callback) {
+		 doSomethingInWebApp = callback;
+	  },
+	  'updateVersion' : function (version) { // window.electronApi.updateVersion(session.version);
+		 ipcRenderer.send('vdonVersion', {ver:version});
+	  },
+	  'updatePPT' : function (PPTHotkey) { // window.electronApi.updateVersion(session.version);
+		 console.log("updatePPT recieved !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		 ipcRenderer.send('PPTHotkey', PPTHotkey);
+	  }
+	});
+} catch(e){console.log("Context isolation might be disabled already");}
+
+var storedEle = null;
 var PPTTimeout = null;
 ipcRenderer.on('postMessage', (event, ...args) => {
 	try {
-		if ("mic" in args[0]) { // this should work for the director's mic mute button as well. Needs to be manually enabled the first time still tho.
-			if (args[0].mic === true) { // unmute
+		if ("record" in args[0]) {
+			if (doSomethingInWebApp) {
+			  var x = args[0].params.x;
+			  var y = args[0].params.y;
+			  var ele = document.elementFromPoint(x,y);
+			  
+			  if (ele.id){
+				  var fauxEvent = {};
+			      fauxEvent.data = {};
+			      fauxEvent.data.record = ele.id;;
+				  doSomethingInWebApp(fauxEvent);
+			  }
+			}
+		}
+		if (doSomethingInWebApp && ("mic" in args[0])){ // this is the new version.
+			var fauxEvent = {};
+			fauxEvent.data = {};
+			fauxEvent.data.mic = args[0].mic 
+			doSomethingInWebApp(fauxEvent);
+			return;
+		} else if ("micOld" in args[0]) { // this is for old pre v22 version
+			if (args[0].micOld === true) { // unmute
 				session.muted = false; // set
 				toggleMute(true); // apply 
-			} else if (args[0].mic === false) { // mute
+			} else if (args[0].micOld === false) { // mute
 				session.muted = true; // set
 				toggleMute(true); // apply
-			} else if (args[0].mic === "toggle") { // toggle
+			} else if (args[0].micOld === "toggle") { // toggle
 				toggleMute();
 			}
 			return;
 		}
 		
-		if (args[0].PPT){
+		if ("PPT" in args[0]){
+			console.log(args[0].PPT);
 			if (PPTTimeout){
 				clearTimeout(PPTTimeout);
-				PPTTimeout = setTimeout(function(){
+				PPTTimeout = setTimeout(function(node){
 					PPTTimeout=null;
-					session.muted = true;
-					toggleMute(true);
-					getById("mutebutton").classList.remove("PPTActive");
-				},200);
+					if (node){
+						session.muted = true;
+						toggleMute(true);
+						getById("mutebutton").classList.remove("PPTActive");
+					} else if (doSomethingInWebApp){
+						var fauxEvent = {};
+						fauxEvent.data = {};
+						fauxEvent.data.PPT = false;
+						doSomethingInWebApp(fauxEvent);
+					}
+				},200, args[0].node);
 			} else {
-				session.muted = false;
-				toggleMute(true);
-				getById("mutebutton").classList.add("PPTActive");
-				PPTTimeout = setTimeout(function(){
-					PPTTimeout=null;
-					session.muted = true;
+				if (args[0].node){
+					session.muted = false;
 					toggleMute(true);
-					getById("mutebutton").classList.remove("PPTActive");
-				},600);
+					getById("mutebutton").classList.add("PPTActive");
+				} else if (doSomethingInWebApp){
+					var fauxEvent = {};
+					fauxEvent.data = {};
+					fauxEvent.data.PPT = true;
+					doSomethingInWebApp(fauxEvent);
+				}
+				PPTTimeout = setTimeout(function(node){
+					PPTTimeout=null;
+					if (node){
+						session.muted = true;
+						toggleMute(true);
+						getById("mutebutton").classList.remove("PPTActive");
+					} else if (doSomethingInWebApp){
+						var fauxEvent = {};
+						fauxEvent.data = {};
+						fauxEvent.data.PPT = false;
+						doSomethingInWebApp(fauxEvent);
+					}
+				},600, args[0].node);
 			}
 			return;
 		}
 		
 		if ("getDeviceList" in args[0]) {
+			
+			var x = args[0].params.x;
+			var y = args[0].params.y;
+			var ele = document.elementFromPoint(x,y);
+			storedEle = ele;
+			var menu = ele.dataset.menu;
+			
+			var response = {};
+			response.menu = menu || false;
+			response.eleId = ele.id || false;
+			response.UUID = ele.dataset.UUID || false;
+			response.params = args[0].params;
+  
 			if (typeof enumerateDevices === "function"){
 				enumerateDevices().then(function(deviceInfos) {
-					deviceInfos = JSON.parse(JSON.stringify(deviceInfos));
-					ipcRenderer.send('deviceList', deviceInfos);
+					response.deviceInfos = deviceInfos;
+					response = JSON.parse(JSON.stringify(response));
+					ipcRenderer.send('deviceList', response);
 				})
 			} else {
 				console.log("calling requestOutputAudioStream");
 				requestOutputAudioStream().then(function(deviceInfos) {
 					
-					deviceInfos = JSON.parse(JSON.stringify(deviceInfos));
+					response.deviceInfos = deviceInfos;
+					response = JSON.parse(JSON.stringify(response));
+					ipcRenderer.send('deviceList', response);
 					
-					var output = [];
+					//deviceInfos = JSON.parse(JSON.stringify(deviceInfos));
+					
+					/* var output = [];
 					for (var i=0;i<deviceInfos.length;i++){
 						if (deviceInfos[i].kind === "audiooutput"){
 							output.push(deviceInfos[i]);
 						}
-					}
+					} */
 					
 					console.log("Should only be audio output");
-					console.log(output);
-					ipcRenderer.send('deviceList', output);
+					//console.log(output);
+					//ipcRenderer.send('deviceList', deviceInfos);
 				})
 			}
 		}
@@ -86,17 +173,32 @@ ipcRenderer.on('postMessage', (event, ...args) => {
 			changeVideoDeviceById(args[0].changeVideoDevice);
 		}
 		
+		if ("nativeList" in args[0]){
+			//console.log("nativeList got");
+			event.returnValue = true;
+		}
+		
 		if ("changeAudioDevice" in args[0]) {
 			changeAudioDeviceById(args[0].changeAudioDevice);
 		}
 		
 		if ("changeAudioOutputDevice" in args[0]) {
-			if (typeof changeAudioOutputDeviceById === "function"){
+			//args[0].data.menu = menu || false;
+			//args[0].data.eleId = ele.id || false;
+			//args[0].data.UUID = ele.dataset.UUID || false;
+			//args[0].deviceInfos;
+			//args[0].data.params = params;
+			if ("data" in args[0]){
+				setSink(storedEle, args[0].changeAudioOutputDevice);
+				storedEle.manualSink = args[0].changeAudioOutputDevice;
+				//storedEle.manualSink = args[0].changeAudioOutputDevice;
+			} else if (typeof changeAudioOutputDeviceById === "function"){
 				changeAudioOutputDeviceById(args[0].changeAudioOutputDevice);
 			} else {
 				changeAudioOutputDeviceByIdThirdParty(args[0].changeAudioOutputDevice);
 			}
-		}
+			storedEle = null;
+		} 
 	} catch(e){
 		console.error(e);
 	}
@@ -116,19 +218,22 @@ function changeAudioOutputDeviceByIdThirdParty(deviceID){
 	
 	document.querySelectorAll("audio, video").forEach(ele=>{
 		try {
-			setSink(ele,deviceID);
-		} catch(e){}
-	});
-	document.querySelectorAll("audio, video").forEach(ele=>{
-		try {
-			setSink(ele,deviceID);
+			if (ele.manualSink){
+				setSink(ele,ele.manualSink);
+			} else {
+				setSink(ele,deviceID);
+			}
 		} catch(e){}
 	});
 	document.querySelectorAll('iframe').forEach( item =>{
 		try{
 			item.contentWindow.document.body.querySelectorAll("audio, video").forEach(ele=>{
 				try {
-					setSink(ele,deviceID);
+					if (ele.manualSink){
+						setSink(ele,ele.manualSink);
+					} else {
+						setSink(ele,deviceID);
+					}
 				} catch(e){}
 			});
 		} catch(e){}
